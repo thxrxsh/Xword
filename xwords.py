2ed91ee7b31479c45d152a1c3db331ecb878437e
Tharusha piyumal <76760967+thxrxsh@users.noreply.github.com>
2023-09-20 17:40:33 +0530

    Create xwords.py

---
+++ xwords.py
@@ -0,0 +1,25 @@
+lst = []
+matrix = {}
+x = 0
+
+output_name = input("Enter the name of outputfile : ")
+
+with open(output_name,"w") as wordlist:
+  while (True):
+    word = input('Enter words and enter 0 to stop :')
+    if (word == '0'):break
+    lst.append(word)
+
+  for i in range(3):
+    matrix[str(i)] = lst
+
+  for a in matrix['0']:
+    wordlist.write(a+"\n")
+
+    for b in matrix['1']:
+      wordlist.write(a+b+"\n")
+    
+      for c in matrix['2']:
+        wordlist.write(a+b+c+"\n")
+
+print("[✓]  Word list created successfuly ")
